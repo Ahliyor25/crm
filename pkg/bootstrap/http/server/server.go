e83916b694c00c86e3bc4d33c365254601f3c242
Ahliyor25 <ahliyor.shodiev@icloud.com>
2023-12-01 17:58:22 +0500

    init

---
+++ pkg/bootstrap/http/server/server.go
@@ -0,0 +1,32 @@
+package server
+
+import (
+	"fmt"
+	"github.com/waffle-frame/clean-architecture-template/pkg/bootstrap/http/router"
+	"github.com/waffle-frame/clean-architecture-template/pkg/config"
+	"go.uber.org/fx"
+	"net"
+	"net/http"
+)
+
+// ServerModule ...
+var ServerModule = fx.Provide(NewServer)
+
+// Dependecies ...
+type Dependecies struct {
+	fx.In
+
+	Config *config.Config
+	Router *router.HTTPRouter
+}
+
+// NewServer ...
+func NewServer(params Dependecies) *http.Server {
+	url := net.JoinHostPort(params.Config.Server.Host, fmt.Sprint(params.Config.Server.Port))
+
+	return &http.Server{
+		MaxHeaderBytes: 32 << 20, // 32 Mb
+		Addr:           url,
+		Handler:        params.Router,
+	}
+}
